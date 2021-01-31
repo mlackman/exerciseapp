3ddf406cc3efab87bcd3c734cc806da415ac66a6
Mika Lackman <mika.lackman@gmail.com>
2021-01-31 20:03:09 +0200

    stuff moved to main.ts

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,15 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import CommandListView from './views/CommandListView.svelte';
-  import SetView from './views/SetView.svelte';
-  import { Command } from './command';
-  import { CommandListViewModel, SetViewModel } from './viewmodel';
+  export let views;
 
   let currentView;
   let currentViewModel;
-
-  const views = {
-    exercisesView: {
-      viewComponent: CommandListView,
-      createViewModel: (exercises) => {
-        const cmds = exercises.map((exercise) => new Command(exercise.name, () => activateView('setView', exercise.sets[0])));
-        return new CommandListViewModel('ExercisesView', cmds);
-      }
-    },
-    workoutsView: {
-      viewComponent: CommandListView,
-      createViewModel: (workouts) => {
-        const cmds = workouts.map((workout) => new Command(workout.name, () => activateView('exercisesView', workout.exercises)));
-        return new CommandListViewModel('WorkoutsView', cmds);
-      }
-    },
-    setView: {
-      viewComponent: SetView,
-      createViewModel: (set) => {
-        return new SetViewModel('SetView', set);
-      }
-    }
-  }
 
   function activateView(viewName: string, data, addToHistory: boolean = true) {
     const view = views[viewName];
     if (!view) { throw new Error(`No such view registered: ${viewName}`); }
     currentView = view.viewComponent;
-    currentViewModel = view.createViewModel(data);
+    currentViewModel = view.createViewModel(data, activateView);
 
     if (addToHistory) {
       window.history.pushState({ viewName, data }, '', '');
@@ -50,46 +24,8 @@
   });
 
   onMount(() => {
-    const program = {
-      workouts: [
-        {
-          id: 1,
-          name: 'Rinta ja ojentajat',
-          exercises: [
-            {
-              id: 1,
-              name: "Penkkipunnerrus",
-              sets: [
-                {
-                  name: "Penkkipunnerrus",
-                  repeats: 5,
-                  weight: 60,
-                }
-              ],
-            },
-          ]
-        },
-        {
-          id: 2,
-          name: 'Selkä ja hauis',
-          exercises: [
-            {
-              id: 2,
-              name: "Leuanveto",
-              sets: [
-                {
-                  name: "Leuanveto",
-                  repeats: 8,
-                  weight: 0,
-                },
-              ],
-            },
-          ]
-        }
-      ]
-    }
-
-    activateView('workoutsView', program.workouts);
+    console.log(views);
+    views.activateInitialView(activateView);
   });
 
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,94 @@
-
 import App from './App.svelte';
+import CommandListView from './views/CommandListView.svelte';
+import SetView from './views/SetView.svelte';
+import { Command } from './command';
+import { CommandListViewModel, SetViewModel } from './viewmodel';
+
+const program = {
+  workouts: [
+    {
+      id: 1,
+      name: 'Rinta ja ojentajat',
+      exercises: [
+        {
+          id: 1,
+          name: "Penkkipunnerrus",
+          sets: [
+            {
+              name: "Penkkipunnerrus",
+              repeats: 5,
+              weight: 60,
+            }
+          ],
+        },
+      ]
+    },
+    {
+      id: 2,
+      name: 'Selkä ja hauis',
+      exercises: [
+        {
+          id: 2,
+          name: "Leuanveto",
+          sets: [
+            {
+              name: "Leuanveto",
+              repeats: 8,
+              weight: 0,
+            },
+          ],
+        },
+      ]
+    }
+  ]
+}
+
+/**
+ * TODO: rename something like AppController
+ * views: {},
+ * activateInitialView etc...
+ */
+const views = {
+  /**
+   * Shows exercises and can select set
+   */
+  exercisesView: {
+    viewComponent: CommandListView,
+    createViewModel: (exercises, activateView) => {
+      const cmds = exercises.map((exercise) => new Command(exercise.name, () => activateView('setView', exercise.sets[0])));
+      return new CommandListViewModel('ExercisesView', cmds);
+    }
+  },
+
+  /**
+   * Shows workouts and can select exercise
+   */
+  workoutsView: {
+    viewComponent: CommandListView,
+    createViewModel: (workouts, activateView) => {
+      const cmds = workouts.map((workout) => new Command(workout.name, () => activateView('exercisesView', workout.exercises)));
+      return new CommandListViewModel('WorkoutsView', cmds);
+    }
+  },
+
+  /**
+   * Shows set from exercise
+   */
+  setView: {
+    viewComponent: SetView,
+    createViewModel: (set, activateView) => {
+      return new SetViewModel('SetView', set);
+    }
+  },
+  activateInitialView: (activateView) => {
+    activateView('workoutsView', program.workouts);
+  }
+}
 
 const app = new App({
 	target: document.body,
 	props: {
+    views: views
 	}
 });
 
