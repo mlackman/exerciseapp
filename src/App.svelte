feb3fc9aa3efb3196246ce3f1f136cf6c76464a6
Mika Lackman <mika.lackman@gmail.com>
2021-10-02 07:13:11 +0300

    exer

--- src/App.svelte
+++ src/App.svelte
@@ -197,6 +197,7 @@
         },
     ]
   */
+    /*
       let exercises = [
         {
             "name": "leuanveto",
@@ -239,7 +240,7 @@
             "weight": "65.0kg",
         },
     ]
-    /*
+    */
     let exercises = [
         {
             "name": "Military Press",
@@ -277,7 +278,6 @@
             "weight": "28 kg",
         },
     ]
-    */
     /*
     let exercises = [
         {
