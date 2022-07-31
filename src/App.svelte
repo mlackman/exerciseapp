04e79d4bb8db8ebe08a6a5bb108810e30c3fe499
Mika Lackman <mika.lackman@finlabs.fi>
2022-07-31 06:55:51 +0300

    exer

--- src/App.svelte
+++ src/App.svelte
@@ -69,6 +69,7 @@
 
     ];
     */
+    /*
     // Arms and abs
     let exercises = [
       {
@@ -112,8 +113,8 @@
         "weight": "17.5kg",
       },
     ];
+    */
 
-    /*
     let exercises = [
       {
         "name": "maastaveto",
@@ -156,7 +157,6 @@
         "weight": "100kg",
       },
     ];
-    */
     /*
     let exercises = [
         {
