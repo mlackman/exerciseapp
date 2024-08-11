d9d5a6aa6c2bc5c0fd3f4b3792bfa6d393c56132
Mika Lackman <mika.lackman@gmail.com>
2024-08-11 13:34:16 +0300

    to vue

---
+++ Gym/src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+  import { useRoute, useRouter } from 'vue-router'
+
+  const router = useRouter();
+
+  function goToTrainingView() {
+    router.push('/training');
+  }
+
+</script>
+
+<template>
+  <main>
+    <ul>
+      <li @click="goToTrainingView()">
+        <h1>Penkki</h1>
+      </li>
+      <li>
+        <h1>Vinopenkki</h1>
+      </li>
+      <li>
+        <h1>Hauiskääntö</h1>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  ul {
+    list-style-type: None;
+  }
+  li {
+    width: 100%;
+    margin-top: 1em;
+    padding: 0.5em;
+    border: solid;
+  }
+</style>
---
+++ Gym/src/views/TrainingView.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1>Penkki 1/5</h1>
+
+    <h2>50 kg</h2>
+
+    <section>
+      <h3>Toistot</h3>
+      <div class="container">
+        <button class="repeat-btn"><</button>
+        <p>8</p>
+        <button class="repeat-btn">></button>
+      </div>
+    </section>
+
+    <div class="container">
+      <button id="rdy-btn">Valmis</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  h2 {
+    font-size: 20vw;
+    text-align: center;
+  }
+
+  .container{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  p {
+    font-size: 20vw;
+  }
+  .repeat-btn {
+    width: 1em;
+    height: 1em;
+    text-align: center;
+    font-size: 5vh;
+    line-height: 0px;
+  }
+  #rdy-btn {
+    font-size: 5vh;
+    margin: auto;
+  }
+</style>
